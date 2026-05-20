1bb3fe9874c3429b1dad6af5c9a510bb56f115b5
Chris <christopherridad@gmail.com>
2026-05-20 18:10:01 -0500

    set up SpotBugs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
 plugins {
     id("java")
+    id("com.github.spotbugs") version "6.0.9"
+}
+
+spotbugs {
+    toolVersion = "4.8.3"
+    effort = com.github.spotbugs.snom.Effort.MAX
+    reportLevel = com.github.spotbugs.snom.Confidence.LOW
+}
+
+tasks.spotbugsMain {
+    reports.create("html") {
+        required = true
+    }
 }
 
 group = "nu.csse.sqe"
