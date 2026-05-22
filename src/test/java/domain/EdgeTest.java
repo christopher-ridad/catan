82b473e9b012da9531a68824db061c642cf82530
Chris <christopherridad@gmail.com>
2026-05-21 19:23:29 -0500

    Constructor_WithLowerBoundaryId_NoExceptionThrown passes

--- src/test/java/domain/EdgeTest.java
+++ src/test/java/domain/EdgeTest.java
@@ -3,8 +3,7 @@ package domain;
 import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class EdgeTest {
 
@@ -15,5 +14,12 @@ public class EdgeTest {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> new Edge(-1, endpoint1, endpoint2));
         assertEquals("Edge id must be between 0 and 71", exception.getMessage());
+    }
+
+    @Test
+    void Constructor_WithLowerBoundaryId_NoExceptionThrown() {
+        Vertex endpoint1 = EasyMock.createMock(Vertex.class);
+        Vertex endpoint2 = EasyMock.createMock(Vertex.class);
+        assertDoesNotThrow(() -> new Edge(0, endpoint1, endpoint2));
     }
 }
