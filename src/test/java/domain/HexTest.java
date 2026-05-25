b4a0fc3e443328a72c6a4a581ab808f652221a3b
arielgarcia55 <arielgarcia3005@gmail.com>
2026-05-24 22:37:44 -0500

    hexConstructor_tokenAtTopOfLowerRange_doesNotThrow passed

--- src/test/java/domain/HexTest.java
+++ src/test/java/domain/HexTest.java
@@ -6,7 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class HexTest {
     @Test
+    void hexConstructor_tokenBelowMin_throwsIllegalArgument() {
+        assertDoesNotThrow(() -> new Hex(TerrainType.FOREST, 1));
+    }
+
+    @Test
     void hexConstructor_tokenAtMin_doesNotThrow() {
         assertDoesNotThrow(() -> new Hex(TerrainType.FOREST, 2));
+    }
+
+    @Test
+    void hexConstructor_tokenAtTopOfLowerRange_doesNotThrow() {
+        assertDoesNotThrow(() -> new Hex(TerrainType.FOREST, 6));
     }
 }
