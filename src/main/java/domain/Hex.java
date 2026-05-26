b1b9756fc5ce1e1e1f9d93977b2634239abc3608
arielgarcia55 <arielgarcia3005@gmail.com>
2026-05-26 10:57:36 -0500

    Made validation logic private

--- src/main/java/domain/Hex.java
+++ src/main/java/domain/Hex.java
@@ -5,14 +5,22 @@ public class Hex {
     private final int numToken;
 
     public Hex(TerrainType terrainType, int numToken) {
+        validateTerrainType(terrainType, numToken);
+        validateNumToken(terrainType, numToken);
+        this.terrainType = terrainType;
+        this.numToken = numToken;
+    }
+
+    private void validateTerrainType(TerrainType terrainType, int numToken) {
         if (terrainType == TerrainType.DESERT && numToken != 0) {
             throw new IllegalArgumentException("Desert hex cannot have a number token");
         }
+    }
+
+    private void validateNumToken(TerrainType terrainType, int numToken) {
         if (terrainType != TerrainType.DESERT && (numToken == 7 || numToken > 12 || numToken < 2)) {
             throw new IllegalArgumentException("Invalid number token: " + numToken);
         }
-        this.terrainType = terrainType;
-        this.numToken = numToken;
     }
 
     public TerrainType getTerrainType() {
