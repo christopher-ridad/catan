b87f506529a11790792a3b5e58e76a128cb61541
isabellasocci <isabellaa.socci@gmail.com>
2026-05-30 01:46:15 -0500

    add ResourceProduction stub and failing tests

---
+++ src/main/java/domain/ResourceProduction.java
@@ -0,0 +1,10 @@
+package domain;
+
+import java.util.List;
+
+public class ResourceProduction {
+
+    public void distributeResources(int roll, List<Vertex> vertices, Bank bank) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
