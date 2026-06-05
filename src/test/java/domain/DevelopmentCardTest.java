58b1b596991587821d202bd3f6fea73131a119cd
Chris <christopherridad@gmail.com>
2026-06-04 20:28:53 -0500

    Constructor_WithValidType_NoExceptionThrown

--- src/test/java/domain/DevelopmentCardTest.java
+++ src/test/java/domain/DevelopmentCardTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DevelopmentCardTest {
@@ -9,5 +10,10 @@ public class DevelopmentCardTest {
     @Test
     void Constructor_WithNullType_ThrowsIllegalArgumentException() {
         assertThrows(IllegalArgumentException.class, () -> new DevelopmentCard(null));
+    }
+
+    @Test
+    void Constructor_WithValidType_NoExceptionThrown() {
+        assertDoesNotThrow(() -> new DevelopmentCard(DevelopmentCardType.KNIGHT));
     }
 }
