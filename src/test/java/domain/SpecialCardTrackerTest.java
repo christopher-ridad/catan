eb30b65479142d936529fb946ccdb72457c16f3b
harrison-smt <harrisonsmith2027@u.northwestern.edu>
2026-06-12 16:49:14 -0500

    instantiated SpecialCardTracker class

---
+++ src/test/java/domain/SpecialCardTrackerTest.java
@@ -0,0 +1,12 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+public class SpecialCardTrackerTest {
+    private SpecialCardTracker specialcardtracker;
+
+    @Test
+    public void test1() {
+        return;
+    }
+}
