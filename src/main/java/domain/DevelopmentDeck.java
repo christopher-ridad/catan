4d48e23731f8a31f30743ba93f7fd57c257d1b8b
Chris <christopherridad@gmail.com>
2026-06-11 23:49:28 -0500

    refactor: extract card initialization into private methods in DevelopmentDeck constructor

--- src/main/java/domain/DevelopmentDeck.java
+++ src/main/java/domain/DevelopmentDeck.java
@@ -9,18 +9,30 @@ public class DevelopmentDeck {
 
     public DevelopmentDeck() {
         cards = new ArrayList<>();
+        addKnightCards();
+        addProgressCards();
+        addVictoryPointCards();
+        Collections.shuffle(cards);
+    }
+
+    private void addKnightCards() {
         for (int i = 0; i < 14; i++) {
             cards.add(new DevelopmentCard(DevelopmentCardType.KNIGHT));
         }
+    }
+
+    private void addProgressCards() {
         for (int i = 0; i < 2; i++) {
             cards.add(new DevelopmentCard(DevelopmentCardType.ROAD_BUILDING));
             cards.add(new DevelopmentCard(DevelopmentCardType.YEAR_OF_PLENTY));
             cards.add(new DevelopmentCard(DevelopmentCardType.MONOPOLY));
         }
+    }
+
+    private void addVictoryPointCards() {
         for (int i = 0; i < 5; i++) {
             cards.add(new DevelopmentCard(DevelopmentCardType.VICTORY_POINT));
         }
-        Collections.shuffle(cards);
     }
 
     public int getRemainingCount() {
